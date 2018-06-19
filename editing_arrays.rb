b70f6bd0e2d8ac0ccd1eb8234edf15e441bfe2d2
Dayarelis Quinones <dayarelis.quinones.17@tfacorps.org>
2018-06-19 20:11:31 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,10 +4,10 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-end
+# def change_rainbow_colors
+#   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+# end
 
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-end
+# def add_colors
+#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+# end
